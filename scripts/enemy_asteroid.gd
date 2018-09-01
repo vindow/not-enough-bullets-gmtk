0bb4a13cd624b40acf730d6cdf0cfd809daa7d6d
Alvin Yang <alvin.yang73@gmail.com>
2018-08-31 22:12:21 -0700

    Added asteroid enemy.

---
+++ scripts/enemy_asteroid.gd
@@ -0,0 +1,12 @@
+extends "res://scripts/enemy.gd"
+
+var spin_velocity = 0.0;
+
+func _ready():
+	randomize()
+	spin_velocity = rand_range(0.5, 1)
+	#velocity = rand_range(600, 800)
+
+func _physics_process(delta):
+	position.y += velocity * delta
+	rotation += spin_velocity * delta
